8787102579bd6ae7336e17c63ac36b9d23d61317
Katerina-KEA <diamondekku@gmail.com>
2023-07-29 11:48:32 +0300

    created formatDate

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -13,7 +13,30 @@ import {
   UserName,
   Date,
 } from './BlogCard.styled';
+ 
+import formatDate from 'helpers/formateDate';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+
+export const BlogCard = ({article}) => {
+  return (
+    <Card key={article.id}>
+      <CardHeader>
+        <CardPoster src={article.poster} alt={article.name} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{article.tag}</Tag>
+        <CardTitle>{article.title}</CardTitle>
+        <CardText>{article.description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={article.avatar} alt={article.name} />
+          <UserInfo>
+            <UserName>{article.name}</UserName>
+            <Date>{formatDate(article.postedAt)}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
---
+++ src/helpers/formateDate.js
@@ -0,0 +1,8 @@
+import { formatDistanceToNow } from 'date-fns';
+
+const formatDate = date => {
+  return formatDistanceToNow(new Date(date), {
+    addSuffix: true,
+  });
+};
+export default formatDate; 
